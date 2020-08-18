ed48a0d7c9cddaa722a7b384e1118d33bacf126b
Denys Kulibaba <50871751+Kulibab@users.noreply.github.com>
2020-08-18 17:36:08 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+let menu = document.getElementById('menu');
+
+menu.onclick = () => {
+    document.getElementById('myTopNav').classList.toggle('responsive');
+    document.getElementById('mainNav').classList.toggle('mb14');
+}
