f662e8d7ec98826a05f675d0687d1c7491a51d1e
Denys Kulibaba <50871751+Kulibab@users.noreply.github.com>
2020-08-27 17:24:42 +0300

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-let menu = document.getElementById('menu');
-
-menu.onclick = () => {
-    document.getElementById('myTopNav').classList.toggle('responsive');
-    document.getElementById('mainNav').classList.toggle('mb14');
-}
